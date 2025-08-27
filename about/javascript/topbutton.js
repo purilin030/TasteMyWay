e73170335261484785b3547bed6898a474b67363
purilin030 <pangleeing@gmail.com>
2025-08-27 17:51:39 +0800

    Delete about directory

--- about/javascript/topbutton.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("backToTopBtn").addEventListener("click", (e) => {
-    e.preventDefault(); // 阻止默认跳转
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
